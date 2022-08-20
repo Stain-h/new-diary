c04318b770dae32785a11e03d91a320ca3e9faa6
Stain-h <sangheon1646@naver.com>
2022-08-20 17:58:01 +0900

    [Add] DateContainer - 오늘 날짜 출력

--- src/containers/DateContainer.jsx
+++ src/containers/DateContainer.jsx
@@ -2,9 +2,11 @@ import React from 'react'
 import styles from './style.module.css'
 
 export default function DateContainer() {
+  const date = new Date();
+
   return (
     <div className={styles.dates}>
-      <p className={styles.today}>2022. 08. 20 (수)</p>
+      <p className={styles.today}> {date.toLocaleDateString('ko-kr')} (수)</p>
     </div>
   )
 }
