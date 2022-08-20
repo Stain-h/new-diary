5e82829fd494a4a81b930a208d6958d2b8aa2cde
Stain-h <sangheon1646@naver.com>
2022-08-20 16:55:11 +0900

    [Add] DateContainer 추가

---
+++ src/containers/DateContainer.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import styles from './style.module.css'
+
+export default function DateContainer() {
+  return (
+    <div className={styles.dates}>
+      <p className={styles.today}>2022. 08. 20 (수)</p>
+    </div>
+  )
+}
--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import DiaryContainer from '../containers/DiaryContainer'
+import DateContainer from '../containers/DateContainer'
 import TodoContainer from '../containers/TodoContainer'
 import styles from './style.module.css'
 
@@ -7,6 +8,7 @@ export default function MainPage() {
   return (
     <div className="">
       <DiaryContainer />
+      <DateContainer />
       <TodoContainer />
       <footer className={styles.footer}>
       Copyright &copy; by sangheon All right reserved. 2022.
