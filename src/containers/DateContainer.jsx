2f4f25a2ee79732dff8025a28768d210e0cfc26f
Stain-h <sangheon1646@naver.com>
2022-08-24 23:22:01 +0900

    [Add] DateContainer 요일 추가

--- src/containers/DateContainer.jsx
+++ src/containers/DateContainer.jsx
@@ -3,10 +3,11 @@ import styles from './style.module.css'
 
 export default function DateContainer() {
   const date = new Date();
+  const weekDay = ['SUN', 'MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT'];
 
   return (
     <div className={styles.dates}>
-      <p className={styles.today}> {date.toLocaleDateString('ko-kr')} (수)</p>
+      <p className={styles.today}> {date.toLocaleDateString('ko-kr')} {weekDay[date.getDay()]}</p>
     </div>
   )
 }
