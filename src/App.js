d4ed74ed6652468521abd00e890d7142309e46a2
Stain-h <sangheon1646@naver.com>
2022-08-27 23:12:05 +0900

    [Add] firebase 초기 세팅

--- src/App.js
+++ src/App.js
@@ -1,6 +1,25 @@
+import { useState, useEffect } from "react";
 import MainPage from "./pages/MainPage";
+import { getFirestore, collection, getDocs, query, where } from "firebase/firestore"
+import {app, db} from './fbase'
 
 export default function App() {
+  // FireStore Set
+  const getData = async() => {
+    const q = query(collection(db, "diary"), where('id', '<=', 20))
+    const querySnapshot = await getDocs(q);
+
+    querySnapshot.forEach(doc => {
+      if(doc.exists()){
+        console.log(`App firestore => ${doc.id}`, doc.data());
+      }
+    })
+  }
+    
+  useEffect(() => {
+    getData()
+  }, [])
+
   return (
     <div>
       <MainPage />
